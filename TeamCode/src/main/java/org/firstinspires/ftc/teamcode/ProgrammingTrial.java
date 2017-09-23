ad81f93d7d987d43d4cfeeeb32b9b9cb5b9a9c9d
Jonathan Ma <jonathanjma@yahoo.com>
2017-09-22 21:51:14 -0700

    Day 1 Test Program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ProgrammingTrial.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.sun.tools.javac.comp.Infer;
+
+@TeleOp(name = "Programming Trial")
+public class ProgrammingTrial extends LinearOpMode
+{
+    private DcMotor motorLeftfront;
+    //private DcMotor motorRightfront;
+   // private DcMotor motorLeftback;
+   // private DcMotor motorRightback;
+
+    @Override
+    public void runOpMode() throws Infer.InferenceException
+    {
+
+        motorLeftfront = hardwareMap.dcMotor.get("motorLeftfront");
+        //motorRightfront = hardwareMap.dcMotor.get("motorRightfront");
+        //motorLeftback = hardwareMap.dcMotor.get("motorLeftback");
+        //motorRightback = hardwareMap.dcMotor.get("motorRightback");
+
+        //motorLeftfront.setDirection(DcMotor.Direction.REVERSE);
+        //motorRightback.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+        while (opModeIsActive())
+        {
+
+            motorLeftfront.setPower(-gamepad1.left_stick_y);
+            //motorRightfront.setPower(-gamepad1.right_stick_y);
+            //motorLeftback.setPower(-gamepad1.left_stick_y);
+            //motorRightback.setPower(-gamepad1.right_stick_y);
+
+            idle();
+        }
+    }
+}
