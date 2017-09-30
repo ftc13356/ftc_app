48bf80ea1e151372f4bc7be1deef03bb575bb644
Krishna <32210045+wolfpak12@users.noreply.github.com>
2017-09-29 21:49:05 -0700

    TeleOp
    Added driving functions in the program. It is a basic one for now. There are a bunch of bugs, though.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTrial.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopTrial.java
@@ -8,27 +8,63 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class teleopTrial extends LinearOpMode
 {
     private DcMotor motorLeftfront;
-    //private DcMotor motorRightfront;
-    //private DcMotor motorLeftback;
-    //private DcMotor motorRightback;
+    private DcMotor motorRightfront;
+    private DcMotor motorLeftback;
+    private DcMotor motorRightback;
 
     @Override
     public void runOpMode() throws InterruptedException
     {
 
         motorLeftfront = hardwareMap.dcMotor.get("motorLeftfront");
-        //motorRightfront = hardwareMap.dcMotor.get("motorRightfront");
-        //motorLeftback = hardwareMap.dcMotor.get("motorLeftback");
-        //motorRightback = hardwareMap.dcMotor.get("motorRightback");
+        motorRightfront = hardwareMap.dcMotor.get("motorRightfront");
+        motorLeftback = hardwareMap.dcMotor.get("motorLeftback");
+        motorRightback = hardwareMap.dcMotor.get("motorRightback");
 
-        //motorLeftfront.setDirection(DcMotor.Direction.REVERSE);
-        //motorRightback.setDirection(DcMotor.Direction.REVERSE);
+        /**public void DriveFW(double power, long time)
+        {
+            motorLeftfront.setPower(-power);
+            motorRightfront.setPower(power);
+            motorLeftback.setPower(-power);
+            motorRightback.setPower(power);
+            Thread.sleep(time);
+        }
+
+        public void DriveLR(double power, long time)
+        {
+            motorLeftfront.setPower(-power);
+            motorRightfront.setPower(-power);
+            motorLeftback.setPower(power);
+            motorRightback.setPower(power);
+            Thread.sleep(time);
+        }
+
+        public void Turn(double power, long time)
+        {
+            motorLeftfront.setPower(power);
+            motorRightfront.setPower(power);
+            motorLeftback.setPower(power);
+            motorRightback.setPower(power);
+            Thread.sleep(time);
+        }
+
+        public void StopMotors()
+        {
+            motorLeftfront.setPower(0);
+            motorRightfront.setPower(0);
+            motorLeftback.setPower(0);
+            motorRightback.setPower(0);
+        }**/
 
         waitForStart();
-        while (opModeIsActive())
+        while (teleopTrial())
         {
 
-            motorLeftfront.setPower(-gamepad1.left_stick_y);
+            DriveFW(1, 5000);
+            Turn(1, 5000);
+            DriveLR(1,5000);
+            StopMotors();
+            //motorLeftfront.setPower(-gamepad1.left_stick_y);
             //motorRightfront.setPower(-gamepad1.right_stick_y);
             //motorLeftback.setPower(-gamepad1.left_stick_y);
             //motorRightback.setPower(-gamepad1.right_stick_y);
