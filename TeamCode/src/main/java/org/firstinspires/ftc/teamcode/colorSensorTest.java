d90971c36d1981c36a682a9898bf8e79eeb2eeae
Ansh Gandhi <ansh.goodboy@gmail.com>
2017-10-22 12:07:27 -0700

    Color Sensor Program
    I wrote the color sensor program that was previously added by accident.
    I also recovered the color sensor autonomous program.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensorTest.java
@@ -43,93 +43,47 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import java.util.Locale;
 
+/*
+ * This is an example LinearOpMode that shows how to use
+ * the REV Robotics Color-Distance Sensor.
+ *
+ * Note that the REV Robotics Color-Distance incorporates two sensors into one device.
+ * It has a light/distance (range) sensor.  It also has an RGB color sensor.
+ * The light/distance sensor saturates at around 2" (5cm).  This means that targets that are 2"
+ * or closer will display the same value for distance/light detected.
+ * In this example, we  also use the distance sensor to display the distance
+ * to the target object.  Note that the distance sensor saturates at around 2" (5 cm).
+ *
+ * It assumes the sensor is configured with the name "sensor_color_distance".
+ */
+
 @Autonomous(name = "colorSensorTest")
+// @Disabled
 public class colorSensorTest extends LinearOpMode {
 
-    /**
-     * Note that the REV Robotics Color-Distance incorporates two sensors into one device.
-     * It has a light/distance (range) sensor.  It also has an RGB color sensor.
-     * The light/distance sensor saturates at around 2" (5cm).  This means that targets that are 2"
-     * or closer will display the same value for distance/light detected.
-     *
-     * Although you configure a single REV Robotics Color-Distance sensor in your configuration file,
-     * you can treat the sensor as two separate sensors that share the same name in your op mode.
-     *
-     * In this example, we represent the detected color by a hue, saturation, and value color
-     * model (see https://en.wikipedia.org/wiki/HSL_and_HSV).  We change the background
-     * color of the screen to match the detected color.
-     *
-     * In this example, we  also use the distance sensor to display the distance
-     * to the target object.  Note that the distance sensor saturates at around 2" (5 cm).
-     *
-     */
     ColorSensor sensorColor;
-    DistanceSensor sensorDistance;
 
     @Override
     public void runOpMode() {
-
-        // get a reference to the color sensor.
         sensorColor = hardwareMap.get(ColorSensor.class, "color");
-
-        // get a reference to the distance sensor that shares the same name.
-        sensorDistance = hardwareMap.get(DistanceSensor.class, "color");
-
-        // hsvValues is an array that will hold the hue, saturation, and value information.
         float hsvValues[] = {0F, 0F, 0F};
-
-        // values is a reference to the hsvValues array.
         final float values[] = hsvValues;
-
-        // sometimes it helps to multiply the raw RGB values with a scale factor
-        // to amplify/attentuate the measured values.
-        final double SCALE_FACTOR = 255;
-
-        // get a reference to the RelativeLayout so we can change the background
-        // color of the Robot Controller app to match the hue detected by the RGB sensor.
         int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
         final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
-
-        // wait for the start button to be pressed.
         waitForStart();
-
-        // loop and read the RGB and distance data.
-        // Note we use opModeIsActive() as our loop condition because it is an interruptible method.
         while (opModeIsActive()) {
-            // convert the RGB values to HSV values.
-            // multiply by the SCALE_FACTOR.
-            // then cast it back to int (SCALE_FACTOR is a double)
-            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
-                    (int) (sensorColor.green() * SCALE_FACTOR),
-                    (int) (sensorColor.blue() * SCALE_FACTOR),
-                    hsvValues);
-
-            // send the info back to driver station using telemetry function.
-            telemetry.addData("Distance (cm)",
-                    String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
+            Color.RGBToHSV((int) (sensorColor.red()), (int) (sensorColor.green()), (int) (sensorColor.blue()), hsvValues);
             telemetry.addData("Alpha", sensorColor.alpha());
             telemetry.addData("Red  ", sensorColor.red());
             telemetry.addData("Green", sensorColor.green());
             telemetry.addData("Blue ", sensorColor.blue());
             telemetry.addData("Hue", hsvValues[0]);
-
-            // change the background color to match the color detected by the RGB sensor.
-            // pass a reference to the hue, saturation, and value array as an argument`
-            // to the HSVToColor method.
-            relativeLayout.post(new Runnable() {
-                public void run() {
-                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-                }
-            });
-
+            if (hsvValues[0]>=340 && hsvValues[0]<=360 || hsvValues[0]>=0 && hsvValues[0]<=20) {telemetry.addData("Color", "Red");}
+            else if (hsvValues[0]>=210 && hsvValues[0]<=275) {telemetry.addData("Color", "Blue");}
+            else {telemetry.addData("Color", "None");}
             telemetry.update();
+            relativeLayout.post(new Runnable() {public void run() {relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));}});
         }
-
-        // Set the panel back to the default color[
-        relativeLayout.post(new Runnable() {
-            public void run() {
-                relativeLayout.setBackgroundColor(Color.WHITE);
-            }
-        });
+        relativeLayout.post(new Runnable() {public void run() {relativeLayout.setBackgroundColor(Color.WHITE);}});
     }
 }
