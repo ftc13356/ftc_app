3288447be18c128b0de3750387528fb1deaddde9
Jonathan Ma <jonathanjma03@gmail.com>
2017-11-03 15:19:34 -0700

    Encoder experimentation Part1 - Teleop Arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoderArm.java
@@ -37,7 +37,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import static android.os.SystemClock.sleep;
 
-@TeleOp(name = "arm with Encoder")
+@TeleOp(name = "Arm with Encoder")
 // @Disabled
 public class encoderArm extends OpMode {
 
@@ -49,8 +49,9 @@ public class encoderArm extends OpMode {
 
     static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
 
-    //Need to change for arm gear tower
+    //Need to change for arm gear tower:
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    //
 
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
