39cfd2c82407d71337ff25caa4d9cf2a57287765
Ansh Gandhi <ansh.goodboy@gmail.com>
2017-11-22 17:22:21 -0800

    Commented/Organized Functions, Added Encoder Function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/basicAutonomousFrame.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/basicAutonomousFrame.java
@@ -41,19 +41,27 @@ import com.qualcomm.robotcore.util.Range;
 @Disabled
 public abstract class basicAutonomousFrame extends LinearOpMode {
 
+    // Defining Motors (Drivetrain)
     public DcMotor motorLeftfront;
     public DcMotor motorRightfront;
     public DcMotor motorLeftback;
     public DcMotor motorRightback;
+
+    // Defining Motor (Arm)
     public DcMotor armMotor;
 
+    // Defining Servos (Arm)
     public Servo clawLeft;
     public Servo clawRight;
 
+    // Defining Variables (Claw)
     double leftPosition = 0;
     double rightPosition = 1;
 
+    // Defining Variables (Drivetrain, Encoder)
     int targetValue = 0;
+
+    // Arm Position Function (Arm, Encoder)
 
     public void armPosition(int position) {
         if (position == 0){
@@ -79,9 +87,19 @@ public abstract class basicAutonomousFrame extends LinearOpMode {
         telemetry.addData("Goal Position", "%7d",targetValue);
     }
 
+
+    // Claw Functions (Arm)
     public void closeClaw() {
         leftPosition = 0.6;
         telemetry.addData("Servo Status", "Closed");
+        clawLeft.setPosition(leftPosition);
+        rightPosition = 1 - leftPosition;
+        clawRight.setPosition(rightPosition);
+    }
+
+    public void partialClaw() {
+        leftPosition = 0.48;
+        telemetry.addData("Servo Status", "Partially Open");
         clawLeft.setPosition(leftPosition);
         rightPosition = 1 - leftPosition;
         clawRight.setPosition(rightPosition);
@@ -95,7 +113,9 @@ public abstract class basicAutonomousFrame extends LinearOpMode {
         clawRight.setPosition(rightPosition);
     }
 
-    //Function 1
+
+
+    // Basic Drive Functions (Drivetrain)
     public void moveForward(double power, long time) {
         motorLeftfront.setPower(-power);
         motorRightfront.setPower(power);
@@ -124,15 +144,8 @@ public abstract class basicAutonomousFrame extends LinearOpMode {
         stop();
     }
 
-    //Function 2
-    public void driveStop(long time) {
-        stop();
-        sleep(time);
-        telemetry.addData("Motors", "Leftfront:0, Rightfront:0, Leftback:0, Rightback:0");
-        telemetry.update();
-    }
 
-    // Defining drive Method (Can Drive in All Directions)
+    // Combined Drive Functions (Drivetrain)
     public void drive(double driveFB, double driveS, double turn, long time, double speedfactor) {
         // Define Speed Variables
         double motorLeftfrontPower;
@@ -156,5 +169,81 @@ public abstract class basicAutonomousFrame extends LinearOpMode {
 
         telemetry.addData("Motors", "Leftfront (%.2f), Rightfront (%.2f), Leftback (%.2f), Rightback (%.2f)", motorLeftfrontPower, motorRightfrontPower, motorLeftbackPower, motorRightbackPower);
         telemetry.update();
+    }
+
+    public void driveStop(long time) {
+        stop();
+        sleep(time);
+        telemetry.addData("Motors", "Leftfront:0, Rightfront:0, Leftback:0, Rightback:0");
+        telemetry.update();
+    }
+
+
+    // Combined Drive Function (Drivetrain, Encoder)
+    public void driveEncoder(double driveFB, double driveS, double turn, double inchFactor, double encoderFactor, double speed) {
+        // Defines Variables
+        int newLeftfrontTarget;
+        int newRightfrontTarget;
+        int newLeftbackTarget;
+        int newRightbackTarget;
+
+        // Calculates Target Position
+        double motorLeftfrontEncoder = (-driveFB + driveS + turn)*encoderFactor;
+        double motorRightfrontEncoder = (driveFB + driveS + turn)*encoderFactor;
+        double motorLeftbackEncoder = (-driveFB - driveS + turn)*encoderFactor;
+        double motorRightbackEncoder = (driveFB - driveS + turn)*encoderFactor;
+
+        // Ensures OpMode is Active
+        if (opModeIsActive()) {
+
+            // Calculates Target Position Based on Current Position
+            newLeftfrontTarget = motorLeftfront.getCurrentPosition() + (int) (motorLeftfrontEncoder * inchFactor);
+            newRightfrontTarget = motorRightfront.getCurrentPosition() + (int) (motorRightfrontEncoder * inchFactor);
+            newLeftbackTarget = motorLeftback.getCurrentPosition() + (int) (motorLeftbackEncoder * inchFactor);
+            newRightbackTarget = motorRightback.getCurrentPosition() + (int) (motorRightbackEncoder * inchFactor);
+
+            // Sets Target Position for Motors
+            motorLeftfront.setTargetPosition(newLeftfrontTarget);
+            motorRightfront.setTargetPosition(newRightfrontTarget);
+            motorLeftback.setTargetPosition(newLeftbackTarget);
+            motorRightback.setTargetPosition(newRightbackTarget);
+
+            // Changes Motor Mode
+            motorLeftfront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorRightfront.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorLeftback.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            motorRightback.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // Sets Power to Motors
+            motorLeftfront.setPower(Math.abs(speed));
+            motorRightfront.setPower(Math.abs(speed));
+            motorLeftback.setPower(Math.abs(speed));
+            motorRightback.setPower(Math.abs(speed));
+
+            // Displays Target and Current Position When Active OpMode and Active Motor(s)
+            while (opModeIsActive() && (motorLeftfront.isBusy() || motorRightfront.isBusy() || motorLeftback.isBusy() || motorRightback.isBusy())) {
+
+                // Displays Target and Current Positions
+                telemetry.addData("Target Value", "Running to %7d :%7d :%7d :%7d", newLeftfrontTarget, newRightfrontTarget, newLeftbackTarget, newRightbackTarget);
+                telemetry.addData("Current Value", "Running at %7d :%7d: %7d :%7d",
+                        motorLeftfront.getCurrentPosition(),
+                        motorRightfront.getCurrentPosition(),
+                        motorLeftback.getCurrentPosition(),
+                        motorRightback.getCurrentPosition());
+                telemetry.update();
+            }
+
+            // Stops Motors
+            motorLeftfront.setPower(0);
+            motorRightfront.setPower(0);
+            motorLeftback.setPower(0);
+            motorRightback.setPower(0);
+
+            // Changes Motor Mode
+            motorLeftfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorRightfront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorLeftback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            motorRightback.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 }
