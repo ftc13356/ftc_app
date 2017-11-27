9af3341b598729187b67189b9dd57351184f158d
Jonathan Ma <jonathanjma03@gmail.com>
2017-11-26 17:48:12 -0800

    Uncommented arm/claw initialization, added vuforia stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomousTopLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomousTopLeft.java
@@ -3,6 +3,11 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 @Autonomous(name = "Autonomous Top Left")
 public class autonomousTopLeft extends autonomousFrame {
 
@@ -13,19 +18,32 @@ public class autonomousTopLeft extends autonomousFrame {
         motorRightfront = hardwareMap.get(DcMotor.class, "motorRightfront");
         motorLeftback = hardwareMap.get(DcMotor.class, "motorLeftback");
         motorRightback = hardwareMap.get(DcMotor.class, "motorRightback");
-        //armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        //clawLeft = hardwareMap.servo.get("clawLeft");
-        //clawRight = hardwareMap.servo.get("clawRight");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        clawLeft = hardwareMap.servo.get("clawLeft");
+        clawRight = hardwareMap.servo.get("clawRight");
 
         motorLeftfront.setDirection(DcMotor.Direction.FORWARD);
         motorRightfront.setDirection(DcMotor.Direction.FORWARD);
         motorLeftback.setDirection(DcMotor.Direction.FORWARD);
         motorRightback.setDirection(DcMotor.Direction.FORWARD);
-        //armMotor.setDirection(DcMotor.Direction.REVERSE);
+        armMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        // parameters.vuforiaLicenseKey = "Put License Key Here";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.FRONT;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        telemetry.addData("Vuforia Status", "Initialized");
+        telemetry.update();
+
+        boolean detect = false;
 
         waitForStart();
 
-        //armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         //To safe zone
         encoderDrive(32, 0, 0, 0.5);
