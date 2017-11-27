c69d1985810719ab7893e5606afba7febfe8b679
Jonathan Ma <jonathanjma03@gmail.com>
2017-11-26 17:42:38 -0800

    Organization

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/armOld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/armOld.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "Arm")
+@TeleOp(name = "ArmOld")
 @Disabled
 public class armOld {
 
