91f744a93ecb97d44fee4d25ed9187f99ca1b5a1
Jonathan Ma <jonathanjma03@gmail.com>
2017-12-02 17:31:05 -0800

    Organized Programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tests/colorSensor/autonomousColorAnsh.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/tests/colorSensor/autonomousColorAnsh.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.autonomous.colorSensor;
+package org.firstinspires.ftc.teamcode.autonomous.tests.colorSensor;
 
 import android.app.Activity;
 import android.graphics.Color;
