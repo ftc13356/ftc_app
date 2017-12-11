551087f58ed15196f82fb04feb1b7cc9e3c2d15f
Ansh Gandhi <ansh.goodboy@gmail.com>
2017-12-10 19:02:45 -0800

    Adding / Removing Unnecessary Import Statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomousBottomLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/autonomousBottomLeft.java
@@ -1,13 +1,8 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 @Autonomous(name = "Autonomous Bottom Left (HouseB)")
 public class autonomousBottomLeft extends autonomousFrame {
