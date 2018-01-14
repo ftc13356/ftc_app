452895b5fef88ec53fcaa40f05f8b85a603b6436
Jonathan Ma <jonathanjma03@gmail.com>
2018-01-13 20:51:34 -0800

    Add comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/debug/armMoveDebug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/debug/armMoveDebug.java
@@ -9,6 +9,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp(name = "Arm Move for Debug")
 public class armMoveDebug extends OpMode{
 
+
+    // Initialize the variables
     private DcMotor armMotor;
     private Servo armClawLeft;
     private Servo armClawRight;
@@ -21,31 +23,43 @@ public class armMoveDebug extends OpMode{
 
     @Override
     public void init() {
+
+        telemetry.addData("Arm", "Initializing");
+
+        // Initialize hardware variables
         armMotor = hardwareMap.dcMotor.get("armMotor");
         armClawLeft = hardwareMap.servo.get("clawLeft");
         armClawRight = hardwareMap.servo.get("clawRight");
         touchSensor = hardwareMap.digitalChannel.get("touchSensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
+
         armMotor.setDirection(DcMotor.Direction.REVERSE);
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        telemetry.addData("Arm", "Initialized");
     }
 
     @Override
     public void loop() {
-        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Left stick is used to raise/lower arm
         armMotorPower = gamepad1.left_stick_y * armSpeedControl;
 
-        if (gamepad1.left_bumper) {
+        // Right bumper opens claw, Left bumper closes claw
+        if (gamepad1.right_bumper) {
             armLeftPosition = 0;
             armRightPosition = 1;
+            telemetry.addData("Arm Servo Status", "Open Completely");
         }
 
-        if (gamepad1.right_bumper) {
+        if (gamepad1.left_bumper) {
             armLeftPosition = 0.7;
             armRightPosition = 0.3;
+            telemetry.addData("Arm Servo Status", "Closed");
         }
 
+        // If arm hits sensor and power is negative, it stops, if positive, it continues
         if (!touchSensor.getState()) {
             telemetry.addData("Touch Sensor", "Is Pressed");
 
@@ -55,7 +69,9 @@ public class armMoveDebug extends OpMode{
             }
         }
 
+        // Set arm power, servo position
         armMotor.setPower(armMotorPower);
+
         armClawLeft.setPosition(armLeftPosition);
         armClawRight.setPosition(armRightPosition);
 
