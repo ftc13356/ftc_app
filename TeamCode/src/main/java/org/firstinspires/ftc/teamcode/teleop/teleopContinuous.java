7b3ce8640c7b3cba573fb170076fbfd9545fa69f
Jonathan Ma <jonathanjma03@gmail.com>
2018-01-26 21:46:02 -0800

    Disabled Programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleopContinuous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/teleopContinuous.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -9,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 ///////////////////////////////////////////////////////////////////////////////
 
 @TeleOp(name = "Teleop Drivetrain and Arm (Continuous Servo)")
+@Disabled
 public class teleopContinuous extends OpMode {
 
     // Creates instances of chassis, glyph claw, and arm
