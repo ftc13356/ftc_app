16089484c8eb5dda623f3702a2ca8e6e72875256
Ansh Gandhi <ansh.goodboy@gmail.com>
2018-10-07 17:54:34 -0700

    Moved files to test package.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/teleop/tests/AWDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/teleop/tests/AWDTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.RoverRuckus.teleop;
+package org.firstinspires.ftc.teamcode.RoverRuckus.teleop.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/teleop/tests/VuforiaNavTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/teleop/tests/VuforiaNavTest.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode.RoverRuckus.teleop;
+package org.firstinspires.ftc.teamcode.RoverRuckus.teleop.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
