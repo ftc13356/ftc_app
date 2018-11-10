ffa213af571a3e8a7801929ce7f877c90aff9efe
Ansh Gandhi <ansh.goodboy@gmail.com>
2018-11-10 14:33:21 -0800

    Idk, it was there for some reason.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoverRuckus/teleop/tests/SingleMotorTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.RoverRuckus.teleop.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "Intake Mechanism Test")
+@Disabled
+public class SingleMotorTest extends OpMode {
+
+    private DcMotor LeftFront;
+
+    public void init() {
+
+        LeftFront = hardwareMap.dcMotor.get("LeftFront");
+    }
+
+    public void loop() {
+        LeftFront.setPower(gamepad1.right_stick_y);
+        telemetry.addData("Motor Speed", 0.2*gamepad1.right_stick_y);
+    }
+}
