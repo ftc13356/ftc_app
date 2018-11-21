c49e9807bf4754b6615f12ee2d7296adcb0e0d92
Jonathan Ma <jonathanjma03@gmail.com>
2018-11-20 22:30:51 -0800

    Added back code for fun java class student code and laptop provisioning guide

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecovery/teleop/outreach/JavaTheBot/studentCode/JavaTheBotAamodVarun.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.RelicRecovery.teleop.outreach.JavaTheBot.studentCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Bobby: Aamod, Varun")
+public class JavaTheBotAamodVarun extends OpMode {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("left");
+        rightMotor = hardwareMap.dcMotor.get("right");
+    }
+
+    @Override
+    public void loop() {
+        leftMotor.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x);
+        rightMotor.setPower(-gamepad1.left_stick_y - gamepad1.left_stick_x);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RelicRecovery/teleop/outreach/JavaTheBot/studentCode/JavaTheBotNathanAndrew.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.RelicRecovery.teleop.outreach.JavaTheBot.studentCode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Jerfur: Nathan, Andrew")
+public class JavaTheBotNathanAndrew extends OpMode {
+
+    DcMotor left;
+    DcMotor right;
+
+    @Override
+    public void init() {
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+    }
+
+    @Override
+    public void loop() {
+        double leftPower;
+        double rightPower;
+        leftPower=(2*gamepad1.left_trigger-1);
+        rightPower=(2*gamepad1.right_trigger-1);
+        if (gamepad1.left_stick_x != 0 || gamepad1.left_stick_y != 0) {
+            leftPower=0;
+            rightPower=0;
+        }
+        left.setPower(leftPower);
+        right.setPower(rightPower);
+    }
+}
